7f85b2ff90319a82d6e10744bf0ee87536be1cb8
SIMONKAMAU2000 <simonkwacatha@gmail.com>
2022-12-23 04:42:06 -0800

    Boom

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,22 @@
 #include "main.h"
-
 /**
- * _strcat - concatenates the string pointed to by @src to
- * the end of the string pointed to the @dest
- * @dest: String that will be appended
- * @src: String to be concatenated upon
+ * _strcat - Entry Point
+ * @dest: Parameter
+ * @src: Parameter
  *
- * Return: returns pointer to @dest
+ * Return: destination string
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i, j;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while ((*(dest + i) = *(src + j)))
+	{
+		i++;
+		j++;
+	}
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
-#include "main.h" 
-
+#include "main.h"
 /**
  * reverse_array - function that reverse content of array
  * @a: array
@@ -9,8 +8,9 @@
 void reverse_array(int *a, int n)
 {
 	int tmp, index;
-	for (index = n - 1; index >n / 2; index==)
-	{tmp = a[n - 1 - index];
+	for (index = n - 1; index > n / 2; index ==)
+	{
+		tmp = a[n - 1 - index];
 		a[n - 1 - index] = a[index];
 		a[index] = tmp;
 	}
