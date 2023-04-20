392cabee2127fddce981dbee003fb4a445643f13
SIMONKAMAU2000 <simonkwacatha@gmail.com>
2023-04-20 05:06:27 -0700

    What is my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name
  * @name: first parameter
  * @f: second parameter
  * Return: nothing
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-	{
 		f(name);
-	}
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,9 @@
-int _putchar(char c);
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
